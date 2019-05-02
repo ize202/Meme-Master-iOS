699d981d2d788ef5ff921defc4a3513bf6f6cc45
Aize Igbinakenzua <aizeakenzua@gmail.com>
2019-05-02 21:17:18 +0100

    - created cocoa touch class of UICollectionViewCell - created IBOutlets

---
+++ Meme-Master/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  Meme-Master
+//
+//  Created by Aize Igbinakenzua on 2019-05-02.
+//  Copyright © 2019 Aize Igbinakenzua. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var emoji: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var levelLabel: UILabel!
+}
